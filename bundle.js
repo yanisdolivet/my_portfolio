2d838bea9ba3a5c44cfa07018e110626d2b5be6c
yanisdolivet <yanis.dolivet@epitech.eu>
2024-04-17 22:29:39 +0200

    [feat]: call the main function into a html file

---
+++ bundle.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './src/app/page';
+
+ReactDOM.render(<Home />, document.getElementById('root'));
+console.log("Hello World")
