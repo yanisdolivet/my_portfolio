8c1954de1f87c789bd2b3007a5ff23d5f4cdbf85
yanisdolivet <yanis.dolivet@epitech.eu>
2024-04-17 20:11:04 +0200

    [feat]: footer section

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import { Section } from "./section";
+
+export const Footer = () => {
+    return <footer className="bg-card">
+        <Section className="py-8">
+            <p className="text-muted-foreground text-sm">
+                @Copyright 2024 Yanis Dolivet
+            </p>
+        </Section>
+    </footer>
+};
