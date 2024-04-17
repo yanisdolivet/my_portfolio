ffa7a2b797d6e000d57762e9741ae38906edab09
yanisdolivet <yanis.dolivet@epitech.eu>
2024-04-17 22:52:06 +0200

    [delete]: index.html & bundle.js ans [feat]: point to the github page url

--- bundle.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './src/app/page';
-
-ReactDOM.render(<Home />, document.getElementById('root'));
-console.log("Hello World")
